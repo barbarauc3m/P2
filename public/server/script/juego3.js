6b1ccca95d60f626a4b07c322aceb60e4144370d
barbarauc3m <100495857@alumnos.uc3m.es>
2025-04-04 12:46:44 +0200

    JUEGO TTREEEEES

---
+++ public/server/script/juego3.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".start-button").addEventListener("click", function() {
+        document.querySelector(".game-title").style.display = "none";
+        document.querySelector(".game-start-container").style.display = "none";
+        document.querySelector(".game-container").style.display = "block";
+
+        // Asegúrate de usar 'url("ruta")' para la imagen de fondo
+        document.body.style.backgroundImage = 'url("../../../images/garden.jpg")';
+
+        // Opcional: Ajustes adicionales para que la imagen se vea bien
+        document.body.style.backgroundSize = "cover"; // Cubre todo el fondo
+        document.body.style.backgroundPosition = "center"; // Centra la imagen
+        document.body.style.backgroundRepeat = "no-repeat"; // Evita repetición
+    });
+});
